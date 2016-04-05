ff83dc547e9390f7170f89aef9c517e9be6a96f4
Audre Staffen <audrewow@gmail.com>
2016-04-04 20:11:31 -0400

    Add HashMap class and GenericHashMap class.

---
+++ cis237hashtable/GenericHashMap.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cis237hashtable
+{
+    class GenericHashMap<TKey, TValue>
+    {
+        private const int PRIMEVALUE = 97;
+        TValue[] theValues;
+
+        public GenericHashMap()
+        {
+            theValues = new TValue[PRIMEVALUE];
+        }
+
+        public TValue Get(TKey key)
+        {
+            int indexForValues = HashFunction(key);
+            return theValues[indexForValues];
+        }
+
+        public void Put(TKey key, TValue value)
+        {
+            int indexForValues = HashFunction(key);
+            theValues[indexForValues] = value;
+        }
+
+        private int HashFunction(TKey key)
+        {
+            return key.GetHashCode() % PRIMEVALUE;
+        }
+    }
+}
